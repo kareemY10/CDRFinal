e8dbfe2d5176d4fb7ff6783e05f38f27d2c4ac04
jabamoha <jabamoha@netanya.ac.il>
2022-05-11 22:21:04 +0300

    Remove HyperLinks From PDF

--- PDFprocessor.py
+++ PDFprocessor.py
@@ -1,68 +1,110 @@
-from bs4 import BeautifulSoup
-import aspose.words as aw
-
-# def ExtractURLS(path):
-#     pdf = pdfx.PDFx(path)
-#     return pdf.get_references_as_dict()
-
-# def CheckUrl(str):
-#  regex = re.compile(
-#         r'^(?:http|ftp)s?://'
-#         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' 
-#         r'localhost|' 
-#         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' 
-#         r'(?::\d+)?' 
-#         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-#  if(re.match(regex,str)is not None):
-#     return True
-#  else:
-#      return False
+import pdfrw
+import re
+import PyPDF2
+import itertools
 
 
-# def Clean_txtFile(myPath):
-#     with open(myPath, encoding='utf-8') as f:
-#         content = f.readlines()
-#     content = [x.strip() for x in content]
-#     new_content = []
-#     url_format='https://'
-#     c=''
-#     for item in content:
-#         print(item)
-#         for i in item.split():
-#             if i.startswith(url_format):
-#                 c='' 
-#             else:
-#                 c=i
-#             new_content.append(c)        
-#         new_content.append('\n')
-#     print(new_content)
-#     with open('result.txt', mode='wt', encoding='utf-8') as myfile:
-#         myfile.write(''.join(new_content))
+
+def Clean_Pdf_From_ClickAbles():
+    pdf = pdfrw.PdfReader("pdf.pdf")
+    new_pdf = pdfrw.PdfWriter()  
+
+    for page in pdf.pages:  
+
+        for annot in page.Annots or []:
+            old_url = annot.A.URI
+            print('old')
+            print(old_url)
+            
+            new_url = pdfrw.objects.pdfstring.PdfString("#")
+            print(new_url)
+            annot.A.URI = new_url
+            print(annot.A.URI)
+        new_pdf.addpage(page)    
+
+    new_pdf.write("new.pdf")
 
 
-def Clean_HyperLinks(myfile):
-    # with open(myfile, 'r') as file:
-    #  data = file.read().rstrip()
-    spam = """<p>Hello world, it's <a href="https://google.com">foo bar time</a></p>
-        <p>Hello world, it's <a href="https://stackoverflow.com">spam eggs</a></p>"""
 
-    soup = BeautifulSoup(spam, 'html.parser')
-    # for a_tag in soup.find_all('a'):
-        # a_tag.replace_with(a_tag.text)
 
-    # print(soup.text)
-    print(soup)
-    print('ok!')
+def remove_pdf_links(file_input, file_output):
+    """ A Simplistic Python function to remove all links from a pdf file
+    """
+ 
+    # Open the pdf file in binary mode and extract contents
+    f = open(file_input, mode='rb')
+    data = f.read()
+    f.close()
+ 
+    # PDF encode URL info as /URI(<<URL PATH>>) so we need a
+    # regex to extract this information
+    regexstring = b"\\/URI \\((.*)\\)"
+    dadsd = b"(www\.)?([^\.]+\.)?(com)?"
+ 
+    # Compile string as regex
+    regexcompiled = re.compile(regexstring, re.MULTILINE)
 
-def Pdf_to_Html(myFile):
-    # Load the PDF file
-    doc = aw.Document(myFile)
+    regexcompiledd = re.compile(dadsd, re.MULTILINE)
+ 
+    # Make a copy of the pdf content for the output pdf file
+    output = data
+ 
+    # Process the pdf contents
+    items = regexcompiled.finditer(data)
 
-    # Save the document as HTML
-    doc.save("Test\Document.html")
-# This is our application entry point
+    together = itertools.chain([regexcompiledd.finditer(data)], items)
+
+ 
+    # For each regex match
+    for match in items:
+        # We have two options here, either replaces the PDF URL with a URL of the same size 
+        # Like So:
+        ## output = output.replace(match.group(1), ' ' * len(match.group(1)), 1)
+        # Or replace the whole command with spaces to remove the URL
+        # Like So:
+        ## output = output.replace(match.group(0), ' ' * len(match.group(0)), 1)
+        #
+        # Experimentation shows that we should leave the ( ) in the encoded format
+        # when we remove the /URI command, so the first option is the best approach
+        output = output.replace(match.group(1), ' ' * len(match.group(1)), 1)
+ 
+    # Because replacing the URL with spaces still leaves the link clickable and
+    # opens a blank webpage in the local browser. We need to replace the 
+    # preceding /URL command to remove the clickable aspect.
+    # Note Experimentation shows that the pdf will crash if the () is not within
+    # pdf /Link tag, so ensure that it is above
+    # Replace might work for this task, but regex was handy so i used it
+    # regex to extract this information
+    regexstring = b"\\/URI"
+ 
+    # Compile string as regex
+    regexcompiled = re.compile(regexstring, re.MULTILINE)
+ 
+    # Process the pdf contents
+    items = regexcompiled.finditer(data)
+ 
+    # For each regex match
+    for match in items:
+        # Replace the /URI tag with spaces
+        output = output.replace(match.group(0), b' ' * len(match.group(0)), 1)
+ 
+    # Save the modified pdf file as a binary file   
+    f = open(file_output, mode='xb')
+    f.write(output)
+    f.close()
+    print('done')
+ 
+ 
+
+ 
+ 
+ 
+
+
 if __name__ == "__main__":
-    my_input ="C:\\temp\\NAC\\FinalProject\\JWP_2021_MoedA.pdf"
-    # Pdf_to_Html(myFile=my_input)
-    Clean_HyperLinks('Test\Document.html')
+    # Clean_Pdf_From_ClickAbles()
+    my_input="./pdf.pdf"
+    my_output="new.pdf"
+    remove_pdf_links('pdf.pdf','new.pdf')
+    print("ok")
     
