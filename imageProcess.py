647f9d1aeba93c1a045850db818482b650af1397
kareemY10 <kareem.skr@gmail.com>
2022-09-11 16:14:44 +0300

    p

--- imageProcess.py
+++ imageProcess.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
-
+from pytesseract import pytesseract
+import os
+import sys
 
 # path to the image or video
 imagename = "image.jpg"
@@ -8,6 +10,7 @@ imagename = "image.jpg"
 # read the image data using PIL
 image = Image.open(imagename)
 
+text = pytesseract. image_to_string(image)
 
 # extract EXIF data
 exifdata = image.getexif()
