fd9a3d394f00a788e04172091cadf994edd7438e
jabamoha <jabamoha@netanya.ac.il>
2022-10-22 18:07:40 +0300

    Disable Macors,Links from docx/docm files

--- DocumentProcessor.py
+++ DocumentProcessor.py
@@ -1,35 +1,66 @@
-# This python file to work with the doucments (.docx type)
 from docx import Document
 from docx.opc.constants import RELATIONSHIP_TYPE as RT
+import aspose.words as aw
+
 
 def DisableLinks(filepath):
-    doc=Document(filepath)
-    rels=doc.part.rels
-    urls=[]
+    print('=================Remove Links===================')
+    doc = Document(filepath)
+    rels = doc.part.rels
+    logs = []
     for rel in rels:
         if rels[rel].reltype == RT.HYPERLINK:
-            urls.append(rels[rel]._target)
+            log = f'[-]Remove the hyperlink ==>{rels[rel]._target}'
+            logs.append(log)
+            print(log)
             doc.part.rels[rel]._target='#'
     doc.save(filepath)
-    print("it's done.")
-    return urls
+    print('[=]Successfully remove all Links.')  
+    print('==============================================================')
+    return logs if logs != None else None 
 
 
-
-def ReadProperties(filepath):
-    document = Document(filepath)
-    # read properties
-    document_property = document.core_properties
-    # print all accessible document properties
-    print([p for p in dir(document_property) if not p.startswith('_')])
-    # print author of the document
-    print(document_property.author)
-    # set new value for title
-    document_property.title = 'Everything is Awesome'
-    # save changes to current file
-    # document.save(path)
-
-
-ReadProperties('C:\\temp\\NAC\\FinalProject\\CDRFP\\CDRFinal\\Test.docm')
+def DisableMacros(filepath,filename):
+    print('================Remove Macros================')
+    doc = aw.Document(filepath)
+    vba_list =list(doc.vba_project.modules)
+    logs =  []
+    for _ in range(len(vba_list)):
+        log = f'[-] Remove the Macro ==>> {doc.vba_project.modules[0].name}'
+        print(log)
+        logs.append(log)
+        doc.vba_project.modules.remove(doc.vba_project.modules[0])
     
+    doc.remove_macros()
+    doc.save(filename+'.docx') 
+    clean_docx(filename+'.docx')
+    print('[=]Successfully remove all Macros.')  
+    print('==============================================================')
+    return logs if logs != None else None 
+       
+
+
+
+# need to fix ... 
+def clean_docx(filepath):
+    clean_footer(filepath)
+    clean_paragraphs(filepath) 
+
+
+
+first_line = 'Evaluation Only. Created with Aspose.Words. Copyright 2003-2022 Aspose Pty Ltd.'
+footer_clean = 'Created with an evaluation copy of Aspose.Words. To discover the full versions of our APIs please visit: https://products.aspose.com/words/'
+
+def clean_footer(filepath):
+    clean_footer(filepath)
+    clean_paragraphs(filepath)    
+
+
+def clean_paragraphs(filepath):
+    doc = Document(filepath)
+    for i in range(len(doc.paragraphs)):
+        doc.paragraphs[i].text = doc.paragraphs[i].text.replace(first_line,'')
+    doc.save(filepath)
+
+
 
