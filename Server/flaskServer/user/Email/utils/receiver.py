99da9708bf331a9dca15ffa2f4bcb4f2e89296c6
jabamoha <jabamoha@netanya.ac.il>
2022-11-22 14:51:58 +0200

    fix

--- Server/flaskServer/user/Email/utils/receiver.py
+++ Server/flaskServer/user/Email/utils/receiver.py
@@ -80,7 +80,3 @@ class EmailReceiver:
         return dict
                     
          
-User = a.user("testmoha99@gmail.com",'gtmzpktjrzdqcywb','imap.gmail.com')
-em = EmailReceiver(User,'Inbox')
-em.receiver()
-print("all done")
